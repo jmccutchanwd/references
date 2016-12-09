991622b19c034f234e4fab86c3bcc7bd49a6d1eb
John McCutchan <john.mccutchan.wd@gmail.com>
2016-12-09 00:05:32 -0600

    initial

---
+++ REF_LISTENERS.js
@@ -0,0 +1,80 @@
+/*===== Team chatty-respected-inteligent-programmers [RIP] ====*/
+/* ================ Begin =============== */
+//
+//================================//
+//      Global Vars               //
+//================================//
+var data;
+var chattyContent = "";
+var newMessage;
+//
+//================================//
+//      Json                      //
+//================================//
+function messageList(e) {
+  data = JSON.parse(e.target.responseText);
+  console.log("JSON loaded ",data);
+  createList();
+}
+//
+var myRequest = new XMLHttpRequest();
+  myRequest.addEventListener("load", messageList);
+  myRequest.open("GET", "messages.json");
+  myRequest.send();
+//
+//================================//
+//  load list into html           //
+//================================//
+function createList(e) {
+  for (var i = 0; i < data.messages.length; i++) {
+    chattyContent += `<li><button class="delete">Delete</button>
+                      ${data.messages[i].name}: ${data.messages[i].message}<br></li>`
+    document.getElementById("putHtmlHere").innerHTML = chattyContent;
+  }
+}
+//
+//===========================================//
+//   listener on Input Field                 //
+//===========================================//
+document.getElementById("inputMessage").addEventListener("keypress", postMessage)
+//
+function postMessage() {
+  if ((event.keyCode == 13) && (document.getElementById("inputMessage").value != "")) {
+    newMessage = document.getElementById("inputMessage").value;
+    console.log("Posted New Message ", newMessage);
+    document.getElementById("putHtmlHere").innerHTML += `<li><button class="delete">Delete</button>
+                                                         ${newMessage}<br></li>`
+    document.getElementById("inputMessage").value = "";
+  }
+}
+//
+//===========================================//
+//   listener on Clear Message Board Button  //
+//===========================================//
+document.getElementById("allClear").addEventListener("click", clear);
+//
+function clear(){
+  // document.getElementById("allClear").disabled = false;
+  // if(document.getElementById("putHtmlHere").innerHTML == ""){
+  //   document.getElementById("allClear").disabled = true;
+  // }
+  document.getElementById("putHtmlHere").innerHTML = "";
+  console.log("Message Board Cleared");
+}
+//
+//================================//
+//  listener on innerHTML Button  //
+//================================//
+document.querySelector("body").addEventListener("click", function(e) {
+  removeListItem(e);
+});
+//
+function removeListItem(e){
+if (e.target.textContent.toLowerCase() === "delete"){
+  e.srcElement.parentElement.remove();
+  console.log("Item deleted ", e.srcElement.parentElement);
+  }
+}
+//
+/* ================ END =============== */
+//

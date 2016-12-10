cf3857fcb3cea38ad31e2b47b67039ea6b751d1b
John McCutchan <john.mccutchan.wd@gmail.com>
2016-12-10 10:48:54 -0600

    updated file

--- element-finder.js
+++ element-finder.js
@@ -2,6 +2,9 @@
 Deploy this to be able to point and click and find the properties of anything in the DOM.
 Results are displayed in the console.
 */
+console.log("Element Finder Loaded");
 document.querySelector("body").addEventListener("click", function(e) {
   console.log(e);
 });
+//
+/* ============ END ================ */

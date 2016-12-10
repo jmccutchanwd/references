7561017331a092df5dcbfa3c35594ff62a1658b8
John McCutchan <john.mccutchan.wd@gmail.com>
2016-12-10 10:46:32 -0600

    Initial

---
+++ element-finder.js
@@ -0,0 +1,7 @@
+/*
+Deploy this to be able to point and click and find the properties of anything in the DOM.
+Results are displayed in the console.
+*/
+document.querySelector("body").addEventListener("click", function(e) {
+  console.log(e);
+});
